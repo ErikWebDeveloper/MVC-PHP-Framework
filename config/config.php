fc11ce520f426d9096f5a575a7782016747e7c19
Erik <erikcodedeveloper@gmail.com>
2024-01-02 19:51:15 +0100

    first commit

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+// App Information
+define('URL', 'http://localhost:80');
+define('APP_NAME', 'My PHP App');
+
+// Database Information
+define('DATABASE_HOST', '');
+define('DATABASE_PORT' , '');
+define('DATABASE_NAME', '');
+define('DATABASE_USER', '');
+define('DATABASE_KEY', '');
+define('DATABASE_CHARSET', 'utf8mb4');
+
+// PHP Mailer Information
+define('HOST' , '');
+define('PORT', );
+define('USER_ACOUNT', '');
+define('KEY_ACOUNT', '');
+
+// Common Paths
+define('MIDDLEWARE', $_SERVER['DOCUMENT_ROOT'] . '/midlewares');
+define('VENDOR_AUTOLOAD', $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+define('EMAILS', $_SERVER['DOCUMENT_ROOT'] . '/emails');
+define('FUNCTIONS', $_SERVER['DOCUMENT_ROOT'] . '/functions/');
+
+// Custom Paths
+?>
+
